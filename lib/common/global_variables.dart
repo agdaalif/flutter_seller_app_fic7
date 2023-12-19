e21ac879b99f94de60859d7a92a0cb39a588ef2a
agdaalif <agdaalif@gmail.com>
2023-12-19 19:09:34 +0700

    meet 14 ad product

--- lib/common/global_variables.dart
+++ lib/common/global_variables.dart
@@ -1,4 +1,3 @@
 class GlobalVariables {
-  static const baseUrl = 'https://8b02-2404-8000-1028-10fb-dc7e-4645-d195-87ba.ngrok.io';
+  static const baseUrl = 'https://9127-2404-8000-1028-10fb-b831-c98c-f209-e5ad.ngrok-free.app';
 }
-    
